e8739ee5553231c072b9c0c6bdf33d95479f6f3e
Huu Quy <quynguyen01688357600@gmail.com>
2024-11-20 22:20:14 +0700

    call api frontend test

--- shop-client/src/App.js
+++ shop-client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+import api from './api/axiosConfig'
+
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [products, setProducts] = useState();
+  const getProducts =async()=>{
+    try {
+      const response = await api.get("/api/v1/products");
+      console.log(response.data);
+      
+    setProducts(response.data);
+      
+    } catch (err){
+      console.log(err);
+
+      
+    }
+    
+
+
+  }
+  useEffect(()=>{
+    getProducts();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
     </div>
   );
 }
