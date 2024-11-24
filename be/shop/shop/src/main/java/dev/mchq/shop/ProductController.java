e4dce7991c57df6811c5fa121870dc2fd9618d66
Huu Quy <quynguyen01688357600@gmail.com>
2024-11-24 11:30:33 +0700

    check cors

--- be/shop/shop/src/main/java/dev/mchq/shop/ProductController.java
+++ be/shop/shop/src/main/java/dev/mchq/shop/ProductController.java
@@ -1,7 +1,6 @@
 package dev.mchq.shop;
 
-import org.apache.coyote.Response;
-import org.bson.types.ObjectId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- be/shop/shop/src/main/java/dev/mchq/shop/ProductRepository.java
+++ be/shop/shop/src/main/java/dev/mchq/shop/ProductRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, ObjectId> {
+public interface ProductRepository extends MongoRepository<Product, String> {
     Optional<Product> findProductByProductId(String productId);
 }
